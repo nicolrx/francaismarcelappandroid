f867e18cc5b361b96e77caa22c4c83894b9277d1
nico_lrx <nicolas.leroux@sciencespo.fr>
2025-05-12 17:29:17 +0200

    splash + icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.llf.francaisavecmarcel"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
